015d6e08322c68785d6658c2e396869ffa6f25da
AhmedFatic <ahmedfatic00@gmail.com>
2026-06-13 22:32:51 +0200

    Webpack konfigurerat och klart

--- webpack.config.js
+++ webpack.config.js
@@ -2,15 +2,19 @@ const path = require('path');
 const CopyWebpackPlugin = require("copy-webpack-plugin");
 
 module.exports = {
+    mode: 'development',
     entry: './src/index.js',
     output: {
         path: path.resolve(__dirname, 'dist'),
-        filename: 'main.js'
+        filename: 'main.js',
+    },
+    experiments: {
+        topLevelAwait: true
     },
     plugins: [
         new CopyWebpackPlugin({
             patterns: [{
-                context: __dirname + '/src/',
+                context: path.resolve(__dirname, 'src'),
                 from: '*.html'
             }]
         })
